26ba2e09d4d91f29d9ec939df96838cba01bed0d
Vinay Mohan Rathod <vinay.rathod@myntra.com>
2020-12-16 17:39:59 +0530

    initial commit

---
+++ src/main/java/com/pg/example/paytmpg/PaytmPgApplication.java
@@ -0,0 +1,13 @@
+package com.pg.example.paytmpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmPgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytmPgApplication.class, args);
+    }
+
+}
